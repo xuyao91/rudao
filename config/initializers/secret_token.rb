b47782c010b23c56442d69b7ecebbbfafd62f420
xy <xy9123@126.com>
2013-08-28 16:45:44 +0800

    项目的基本文件生成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rudao::Application.config.secret_token = '38451b6fc714cce792ca080b1cf963502afff9e89c5fc56ecb9283fb1a53b5dbcf1695b9574082ba9c95a005e6d1cc42a71073da113846b399c1917fa9544964'
