ad8fd8bfc65b97193368e91c3549606b7a032ebd
xy <xy9123@126.com>
2013-08-29 17:49:54 +0800

    Signed-off-by: xy <xy9123@126.com>

--- src/app/models/admin.rb
+++ src/app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ActiveRecord::Base
   # attr_accessible :title, :body
-  
+    acts_as_paranoid
    #acts_as_authentic 
   attr_accessible :login, :email, :password, :password_confirmation
 end
