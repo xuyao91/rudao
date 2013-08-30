3d493553bd8383ff54f6db3137e56a9873119455
xy <xy9123@126.com>
2013-08-30 09:16:29 +0800

    Signed-off-by: xy <xy9123@126.com>

--- src/app/models/shop.rb
+++ src/app/models/shop.rb
@@ -1,4 +1,5 @@
 class Shop < ActiveRecord::Base
   # attr_accessible :title, :body
   acts_as_paranoid
+  has_many :user_shops, :conditions => ["user_shops.deleted_at is null"]
 end
--- src/app/models/user_shop.rb
+++ src/app/models/user_shop.rb
@@ -1,4 +1,6 @@
 class UserShop < ActiveRecord::Base
   # attr_accessible :title, :body
   acts_as_paranoid
+  belongs_to :user, :conditions => ["users.deleted_at is null"]
+  belongs_to :shop, :conditions => ["shops.deleted_at is null"]
 end
